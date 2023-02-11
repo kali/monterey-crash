ecdc72aeaa7bc93609ae75a5e6a232db127fc602
Mathieu Poumeyrol <mathieu@poumeyrol.fr>
2023-02-11 11:21:04 +0100

    missing main

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#![allow(dead_code)]
+#[derive(Copy, Clone)]
+enum BinOp {
+    Min,
+}
+#[derive(Clone, Copy)]
+enum OutputStoreSpec {
+    View(usize),
+    Strides([isize; 5])
+}
+#[derive(Clone)]
+enum ProtoFusedSpec {
+    BinScalar(AttrOrInput, BinOp),
+    BinPerRow(AttrOrInput, BinOp),
+    BinPerCol(AttrOrInput, BinOp),
+    AddRowColProducts(AttrOrInput, AttrOrInput),
+    AddUnicast(OutputStoreSpec, AttrOrInput),
+    Store,
+}
+#[derive(Clone)]
+enum AttrOrInput {
+    Attr(std::sync::Arc<()>),
+    Input(usize),
+}
+fn main() {
+    let mut stuff = vec!(vec!(1));
+    for i in 0..50000 {
+	let len = (stuff[i].len() * 134775813) % 4096;
+        stuff.push((1234123414u32..).take(len).collect());
+    }
+    std::mem::drop(stuff);
+    let _ = vec!((Box::new(()), vec![ProtoFusedSpec::Store])).as_slice().to_owned();
+}
